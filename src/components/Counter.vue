2374bde588d9e5b7c5f09eab71f3a52489495e80
Ezequiel Rango <ERango@proveedor.teco.com.ar>
2023-03-09 16:23:49 -0300

    first commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>Counter Vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h2>Computed: {{ countComputed }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button>random</button>
+    <h1>mapState</h1>
+    <h1>mapState: {{ count }}</h1>
+    <h2>lastMutation: {{ lastMutation }}</h2>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+    
+
+    computed: {
+        countComputed(){
+            return this.$store.state.count
+        },
+        // ...mapState(['count'])
+        ...mapState(['count', 'lastMutation'])
+    },
+
+    methods: {
+        increment(){
+            this.$store.commit('increment')
+        },
+        incrementBy(){
+            this.$store.commit('incrementBy', 5)
+        },
+    }
+
+}
+</script>
